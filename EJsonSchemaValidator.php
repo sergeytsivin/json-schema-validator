291a77644559d84baadc3cdf0b47367395bdf26a
Sergey Tsivin <stsivin@rbc.ru>
2014-05-12 13:05:35 +0400

    Implemented and tested utoloading of classes from the library

--- EJsonSchemaValidator.php
+++ EJsonSchemaValidator.php
@@ -13,12 +13,37 @@ class EJsonSchemaValidator extends CApplicationComponent
     /**
      * @var string Path alias to Json Schema for PHP library
      */
-    public $jsonSchemaPathAlias = 'application.vendor.justinrainbow.json-schema.src';
+    public $libraryPathAlias = 'application.vendor.justinrainbow.json-schema.src.JsonSchema';
+    public $schemaFilesPathAlias = 'application.data.json-schemes';
 
-    function init()
+    public function init()
     {
-        Yii::setPathOfAlias('JsonSchema', Yii::getPathOfAlias($this->jsonSchemaPathAlias));
+        // We can use standard Yii autoloader, because the library is PSR-0 compliant
+        // We just need to create a path alias:
+        Yii::setPathOfAlias('JsonSchema', Yii::getPathOfAlias($this->libraryPathAlias));
 
         return parent::init();
+    }
+
+    /**
+     * Validate the JSON file pointed to by $dataUri agains the $schemaFile
+     * @todo Implement
+     * @param $dataUri
+     * @param $schemaFile
+     * @return bool
+     */
+    public function validate($dataUri, $schemaFile)
+    {
+        return true;
+    }
+
+    /**
+     * Resolves schema file name to the fully qualified path
+     * @param $fileName
+     * @return string
+     */
+    public function resolveSchemaPath($fileName)
+    {
+        return Yii::getPathOfAlias($this->schemaFilesPathAlias) . DIRECTORY_SEPARATOR . $fileName;
     }
 }
